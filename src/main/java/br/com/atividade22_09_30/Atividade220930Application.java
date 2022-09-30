e7658398c9655284cc4b61c586e1024c2a91b164
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-09-30 20:23:38 -0300

    adicionados pacotes para MVC

---
+++ src/main/java/br/com/atividade22_09_30/Atividade220930Application.java
@@ -0,0 +1,13 @@
+package br.com.atividade22_09_30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade220930Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Atividade220930Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/atividade22_09_30/Atividade220930ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atividade22_09_30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade220930ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
